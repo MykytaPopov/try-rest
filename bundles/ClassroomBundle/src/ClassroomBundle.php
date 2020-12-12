cd404957cb0b92cc99a3ab9beaf708b90504ea8a
Mykyta Popov <mr.mykyta.popov@gmail.com>
2020-12-12 10:07:27 +0000

    create and install classroom bundle

---
+++ bundles/ClassroomBundle/src/ClassroomBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inner\ClassroomBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ClassroomBundle extends Bundle
+{
+
+}
---
+++ bundles/ClassroomBundle/src/Entity/Classroom.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inner\ClassroomBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Inner\ClassroomBundle\Repository\ClassroomRepository;
+
+/**
+ * The classroom entity
+ *
+ * @ORM\Entity(repositoryClass=ClassroomRepository::class)
+ */
+class Classroom
+{
+    /**
+     * The unique identifier of the classroom
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * The unique name of the classroom
+     *
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * The date of the classroom creation
+     *
+     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
+     */
+    private $createdAt;
+
+    /**
+     * The active status of the classroom
+     *
+     * @ORM\Column(type="boolean")
+     */
+    private $isActive;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): self
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+}
